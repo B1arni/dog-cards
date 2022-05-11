d25a305c6e22f93c92621df5dc7730ca2a9f37d0
B1arni <haraldironbutt@gmail.com>
2022-05-11 11:39:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            card: null
+        };
+        this.changeImageHandler = this.changeImageHandler.bind(this)
+    }
+
+    setImage () {
+        return fetch('https://dog.ceo/api/breeds/image/random').then(response => response.json()).then(data => {
+           this.setState({
+               card: data.message,
+           })
+       });
+    }
+
+    componentDidMount() {
+        this.setImage();
+    }
+
+    changeImageHandler() {
+        this.setImage();
+    }
+
+    render () {
+        return (
+            <div className="app">
+                <img className='image' src={this.state.card}/>
+                <button className="button" onClick={this.changeImageHandler}>Change image</button>
+            </div>
+        )
+    }
 }
 
 export default App;
